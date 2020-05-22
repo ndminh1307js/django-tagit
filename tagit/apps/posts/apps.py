a45e47f61c2ef89ffeae52f21955bb530ffb5617
ndminh1307js <ndminh1307js@gmail.com>
2020-05-22 18:17:50 +0700

    Add signals

--- tagit/apps/posts/apps.py
+++ tagit/apps/posts/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class PostsConfig(AppConfig):
     name = 'posts'
+
+    def ready(self):
+        # import signal handlers
+        import tagit.apps.posts.signals
